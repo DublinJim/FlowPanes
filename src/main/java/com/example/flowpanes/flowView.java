1f639c93728f244692baf55dd526abb7267190db
James Keogh <jkeogh@adga.ca>
2022-08-25 09:41:39 -0400

    adds and neatens the button interface

--- src/main/java/com/example/flowpanes/flowView.java
+++ src/main/java/com/example/flowpanes/flowView.java
@@ -1,11 +1,25 @@
 package com.example.flowpanes;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 
-public class flowView {
+public class flowView implements Initializable {
+
 
     @FXML
-    private Label lbl1;
+    private FlowPane flowpane1;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+        flowpane1.setOrientation(Orientation.VERTICAL);
+    }
+
 
 }
