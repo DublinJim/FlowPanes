b61be15cff104c8f4306523daf935df41336189d
James Keogh <jkeogh@adga.ca>
2022-08-25 09:50:06 -0400

    adds a fifth button

--- src/main/java/com/example/flowpanes/flowView.java
+++ src/main/java/com/example/flowpanes/flowView.java
@@ -15,13 +15,19 @@ public class flowView implements Initializable {
     public Button btn2;
     public Button btn3;
     public Button btn4;
+    public Button btn5;
     @FXML
     private FlowPane flowpane1;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-btn1.setText("One");
+        btn1.setText("One");
+        btn2.setText("Two");
+        btn3.setText("Three");
+        btn4.setText("Four");
+        btn5.setText("Five");
+
         flowpane1.setOrientation(Orientation.VERTICAL);
     }
 
