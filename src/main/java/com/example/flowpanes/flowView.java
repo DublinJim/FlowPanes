f7c7d18ec1d132fcd223b84b396e6b01a63b7b9a
James Keogh <jkeogh@adga.ca>
2022-08-25 09:45:24 -0400

    removes the fxml property from buttons

--- src/main/java/com/example/flowpanes/flowView.java
+++ src/main/java/com/example/flowpanes/flowView.java
@@ -5,19 +5,23 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Orientation;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 
 public class flowView implements Initializable {
 
 
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public Button btn4;
     @FXML
     private FlowPane flowpane1;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-
+btn1.setText("One");
         flowpane1.setOrientation(Orientation.VERTICAL);
     }
 
